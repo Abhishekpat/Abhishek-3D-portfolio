fd41e96ac7852fd68fea426623060d555816f54a
Abhishekpat <104193075+Abhishekpat@users.noreply.github.com>
2025-10-04 03:12:56 +0530

    Update ProfileCard.jsx for image showing

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -25,7 +25,7 @@ const adjust = (value, fromMin, fromMax, toMin, toMax) =>
 const easeInOutCubic = x => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2);
 
 const ProfileCardComponent = ({
-  avatarUrl = '/image.png',
+  avatarUrl = './image.png',
   iconUrl = '<Placeholder for icon URL>',
   grainUrl = '<Placeholder for grain URL>',
   behindGradient,
